7e7838b21469475f5a91b2448a59af3df9aaba1e
julgos <95368112+julgos@users.noreply.github.com>
2026-01-04 16:41:08 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+// Prosty Service Worker do obsługi PWA
+const CACHE_NAME = 'solar-invest-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
